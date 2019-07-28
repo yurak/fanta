dd73e0f59dacd13fddab00be1f83b7e48e3a89cd
Yura Kuzhiy <yurakuzhiy@gmail.com>
2019-07-28 10:06:10 +0300

    Small cleanup

--- app/models/slot.rb
+++ app/models/slot.rb
@@ -1,5 +1,2 @@
 class Slot < ApplicationRecord
-  def mantra_positions
-    Position.where(name: position.split('/'))
-  end
 end
--- app/models/team_module.rb
+++ app/models/team_module.rb
@@ -2,8 +2,4 @@ class TeamModule < ApplicationRecord
   has_many :slots, dependent: :destroy
 
   validates :name, uniqueness: true
-
-  def lines
-    [1].concat(name.split('-').map(&:to_i))
-  end
 end
