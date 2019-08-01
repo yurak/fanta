b86e402353fd050769e1ac011f726a3ed689e440
Bohdan Chaban <bohdan.chaban@itv.com>
2019-08-01 14:17:46 +0300

    Add rules page, links validations

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,2 +1,7 @@
 class Link < ApplicationRecord
+  URL_REGEXP = /\A(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?\Z/ix
+
+  validates :name, presence: true
+  validates :url, presence: true
+  validates :url, format: { with: URL_REGEXP, message: 'is invalid' }
 end
