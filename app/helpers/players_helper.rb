084bf1afd6fcb64e3d6333dcbd69d1278e5f2b50
Yura Kuzhiy <yurakuzhiy@gmail.com>
2019-08-03 18:08:54 +0300

    Show only available players in slect

--- app/helpers/players_helper.rb
+++ app/helpers/players_helper.rb
@@ -7,4 +7,19 @@ module PlayersHelper
       ]
     end
   end
+
+  def available_for_select_by_position(team, position: nil)
+    if position
+      scope = team.players.includes(:positions).where(positions: { name:  position.split('/')})
+    else
+      scope = team.players
+    end
+
+    scope.order_by_status.collect do |x|
+      klass = x.status
+      [
+        "(#{x.positions.map(&:name).join('-')}) #{x.name} (#{x.status})", x.id, { class: klass}
+      ]
+    end
+  end
 end
