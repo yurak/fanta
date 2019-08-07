2cd8abab6233f723f7686f4e560c86556b5b057f
Bohdan Chaban <bohdan.chaban@itv.com>
2019-08-07 12:50:04 +0300

    Add logo to team#show page

--- app/models/team.rb
+++ app/models/team.rb
@@ -5,8 +5,6 @@ class Team < ApplicationRecord
 
   validates :name, uniqueness: true
 
-  has_many :lineups, dependent: :destroy
-
   def logo_path
     if File.exist?("app/assets/images/teams/#{name}.png")
       "teams/#{name}.png"
