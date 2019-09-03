9d4bf9d002ffb05da10160d7f7858eb7935cdaad
Bohdan Chaban <bohdan.chaban@itv.com>
2019-09-03 16:37:12 +0300

    Update MatchPlayer total_score count

--- app/models/match_player.rb
+++ app/models/match_player.rb
@@ -18,16 +18,23 @@ class MatchPlayer < ApplicationRecord
   def total_score
     return 0 unless score
     total = score
+
+    # bonuses
+    total += goals*3 if goals
+    total += caught_penalty*3 if caught_penalty
+    total += scored_penalty*2 if scored_penalty
+    total += assists if assists
     total += 1 if cleansheet
+
+    # maluses
+    total -= missed_goals*2 if missed_goals
+    total -= missed_penalty if missed_penalty
+    total -= failed_penalty*3 if failed_penalty
+    total -= own_goals*2 if own_goals
     total -= 0.5 if yellow_card
     total -= 1 if red_card
-
-    total += goals*3 if goals
-    total += assists if assists
-    total -= missed_goals*2 if missed_goals
-
-    total -= malus if malus
-    total += bonus if bonus
+    total -= position_malus if position_malus
+    # total += bonus if bonus
 
     return total
   end
