e962532af542cdcd5a71d105dc5e5c7df5faaedb
Yura Kuzhiy <yurakuzhiy@gmail.com>
2020-01-07 11:57:16 +0200

    More specs

--- spec/models/match_player_spec.rb
+++ spec/models/match_player_spec.rb
@@ -23,6 +23,16 @@ describe MatchPlayer, type: :model do
       let(:match_player) { create :match_player, goals: 3, caught_penalty: 1, scored_penalty: 2, cleansheet: 3 }
       it { is_expected.to eq 17 }
     end
+
+     context 'scored goals and penalties and cleansheets and cards' do
+      let(:match_player) { create :match_player, goals: 3, caught_penalty: 1, scored_penalty: 2, cleansheet: 3 }
+      it { is_expected.to eq 17 }
+    end
+
+    context 'scored goals and penalties and cleansheets and cards' do
+      let(:match_player) { create :match_player, goals: 3, caught_penalty: 1, cleansheet: 3, yellow_card: 1, red_card: 1 }
+      it { is_expected.to eq 11.5 }
+    end
   end
 end
 
