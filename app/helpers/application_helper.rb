30e3e3e3ba56f973b0b281e835c7a86ad76b5488
Bohdan Chaban <b.chaban.91@gmail.com>
2020-02-28 14:34:21 +0200

    Update League Table page UI

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -30,14 +30,6 @@ module ApplicationHelper
     end
   end
 
-  def result_status_class(status)
-    case status
-    when 'W' then 'badge-success'
-    when 'D' then 'badge-secondary'
-    when 'L' then 'badge-danger'
-    end
-  end
-
   def lineup_class(status)
     status ? 'lineup-completed' : 'lineup-uncompleted'
   end
--- app/models/result.rb
+++ app/models/result.rb
@@ -1,6 +1,9 @@
 class Result < ApplicationRecord
   belongs_to :team
 
+  scope :by_league, ->(league_id) { joins(:team).where(teams: {league_id: league_id}) }
+  scope :ordered, -> { order(points: :desc).order(Arel.sql('scored_goals - missed_goals desc')).order(scored_goals: :desc) }
+
   delegate :lineups, to: :team
 
   def matches_played
@@ -12,6 +15,6 @@ class Result < ApplicationRecord
   end
 
   def form
-    @form ||= lineups.closed.limit(5).map(&:result)
+    @form ||= lineups.closed.limit(5).map{|l| [l.result, l.match_result, l.opponent.code_name, l.tour_id]}
   end
 end
