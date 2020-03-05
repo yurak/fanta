03b394071660f5a770d89b057e6bb0dc4228c468
Yura Kuzhiy <yurakuzhiy@gmail.com>
2020-03-05 18:09:36 +0800

    Remove outdated seeder

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,23 +8,3 @@
 
 p 'Create Tournaments'
 TournamentCreator.call
-
-# p 'Create Clubs'
-# Seeder.new(objects_name: 'clubs').save
-#
-# p 'Create Teams'
-# Seeder.new(objects_name: 'teams').save
-#
-# p 'Create Players'
-# Seeder.new.teams.each do |team|
-#   Seeder.new(team_name: team).create_team
-# end
-#
-# p 'Create Users'
-# UserCreator.call
-#
-# p 'Create Tours'
-# CalendarCreator.call
-#
-# p 'Create Result records'
-# ResultsManager.new.create
