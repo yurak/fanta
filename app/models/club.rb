8996cec457afe9163ed7f800c1e42a82e4e9b166
Bohdan Chaban <b.chaban.91@gmail.com>
2020-03-16 00:19:06 +0200

    Add EPL club logos, update clubs ui

--- app/models/club.rb
+++ app/models/club.rb
@@ -7,7 +7,7 @@ class Club < ApplicationRecord
 
   scope :order_by_players_count, -> { includes(:players).left_joins(:players).group(:id).order(Arel.sql('COUNT(players.id) DESC')) }
 
-  def code
-    name[0..2].upcase
+  def logo_path
+    "clubs/#{name.downcase.tr(' ', '_')}.png"
   end
 end
