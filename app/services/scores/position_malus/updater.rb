c1611d009abf4a05f6b175963c21f261c197ec03
Bohdan Chaban <bohdan.chaban@itv.com>
2020-06-28 00:39:36 +0300

    Update PositionMalus service (#138)

--- app/services/scores/position_malus/updater.rb
+++ app/services/scores/position_malus/updater.rb
@@ -9,7 +9,7 @@ module Scores
         @tour.match_players.main.with_score.each do |mp|
           next unless mp.position_malus?
 
-          mp.update(position_malus: malus(mp))
+          mp.update(position_malus: malus(mp)) if mp.score.positive?
         end
       end
 
