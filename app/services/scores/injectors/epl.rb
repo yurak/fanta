a133a98e210bd242d05110d299b144d0b0ab9e1b
Bohdan Chaban <bohdan.chaban@itv.com>
2020-07-11 22:38:03 +0300

    EPL scores injector, update Player profile UI (#153)
    * Implement EPL Scores Injector
    
    * Add reservists positions at Lineup page
    
    * Update Player Profile UI

--- app/services/scores/injectors/epl.rb
+++ app/services/scores/injectors/epl.rb
@@ -1,6 +1,7 @@
 module Scores
   module Injectors
     class Epl < ApplicationService
+      URL = 'http://132.145.254.115:8080/scrapper/mantra/rounds?round='.freeze
       attr_reader :tour
 
       def initialize(tour: nil)
@@ -8,8 +9,25 @@ module Scores
       end
 
       def call
-        # TODO: implement scores injector for EPL
-        # raise NotImplementedError
+        tour.match_players.each do |mp|
+          mp_pseudo = mp.player.pseudo_name.downcase
+          player_data = scores_arr.detect { |pl| pl['name'].mb_chars.normalize(:kd).gsub(/[^\x00-\x7F]/n, '').downcase.to_s == mp_pseudo }
+          mp.update(score: player_data['stat'].to_f) if player_data && player_data['stat'].to_f.positive?
+        end
+      end
+
+      private
+
+      def scores_arr
+        @scores_arr ||= JSON.parse(request)
+      end
+
+      def request
+        RestClient.get(tour_url)
+      end
+
+      def tour_url
+        "#{URL}#{tour.real_number}"
       end
     end
   end
