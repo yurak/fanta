5408e8ed7aa56207aa6258a021905d24b791b4b6
Bohdan Chaban <b.chaban.91@gmail.com>
2020-07-12 20:56:48 +0300

    Add season factory

--- spec/factories/league.rb
+++ spec/factories/league.rb
@@ -2,6 +2,7 @@ FactoryBot.define do
   factory :league do
     sequence(:name) { |i| "name#{i}" }
     association :tournament
+    association :season
 
     trait :serie_a do
       association :tournament, :serie_a
