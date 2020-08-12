7960a6de52e906c2fb30ae3727808c1d4899151a
Bohdan Chaban <b.chaban.91@gmail.com>
2020-08-13 00:16:14 +0300

    Add ua locale

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,13 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  around_action :switch_locale
 
   private
+
+  def switch_locale(&action)
+    locale ||= params[:locale] || I18n.default_locale
+    I18n.with_locale(locale, &action)
+  end
 
   def after_sign_in_path_for(resource)
     stored_location_for(resource) || articles_path
--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,8 @@ module Fanta
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
+    config.i18n.default_locale = :en
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
