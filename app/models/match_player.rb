683f310c2838237f57a75da1a5520cb5f3dcd9a8
Bohdan Chaban <b.chaban.91@gmail.com>
2020-09-09 19:12:38 +0300

    Small fixes

--- app/models/match_player.rb
+++ app/models/match_player.rb
@@ -91,6 +91,8 @@ class MatchPlayer < ApplicationRecord
     return 0 unless (real_position_arr & Position::CLEANSHEET_ZONE).present?
     return 0 unless (position_names & Position::CLEANSHEET_ZONE).present?
 
+    return 0 if !league.custom_bonuses && (real_position_arr & Position::CLASSIC_CLEANSHEET_ZONE).present?
+
     if league.custom_bonuses && real_position_arr.include?(Position::MEDIANO) && position_names.include?(Position::MEDIANO)
       CUSTOM_CLEANSHEET_BONUS
     else
