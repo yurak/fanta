2a4a6987f570839d12fc7de2d582fb2d3ed06edd
Bohdan_Chaban@epam.com <b.chaban.91@gmail.com>
2021-03-05 15:46:10 +0200

    Add rake gem to gemfile

--- Gemfile
+++ Gemfile
@@ -3,6 +3,7 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby '2.5.1'
 
+gem 'rake'
 gem 'rails', '~> 5.2.3'
 gem 'sqlite3'
 gem 'puma', '3.12.6'
--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid for current version and patch releases of Capistrano
-lock "~> 3.14.1"
+lock "~> 3.15.0"
 
 set :application, "fanta"
 set :repo_url, "https://github.com/yurak/fanta"
