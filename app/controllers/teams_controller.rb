1e6917c948fcfbe434818ebf250d6fb00e2fe685
Yura Kuzhiy <yurakuzhiy@gmail.com>
2021-08-16 14:15:40 +0300

    Open api (#248)

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,11 +1,16 @@
 class TeamsController < ApplicationController
   skip_before_action :authenticate_user!, only: %i[show]
 
-  respond_to :html
+  respond_to :html, :json
 
   helper_method :team
 
-  def show; end
+  def show
+    respond_to do |format|
+      format.html
+      format.json { render json: team.players.to_json }
+    end
+  end
 
   private
 
