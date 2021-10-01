da3b3f2d131b96f01361d63e945fc2fc6fb9d227
Bohdan Chaban <b.chaban.91@gmail.com>
2021-10-01 21:22:35 +0300

    Fix Last purchases at auction page

--- app/models/transfer.rb
+++ app/models/transfer.rb
@@ -5,7 +5,7 @@ class Transfer < ApplicationRecord
 
   enum status: { incoming: 0, outgoing: 1 }
 
-  default_scope { includes(:player) }
+  default_scope { includes(%i[player team]) }
 
   scope :by_league, ->(league_id) { where(league_id: league_id) }
   scope :by_player, ->(player_id) { where(player_id: player_id) }
