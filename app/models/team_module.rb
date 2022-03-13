9a6ec3d5f94c7a4651d999736eea233bacd4c22c
Bohdan Chaban <b.chaban.91@gmail.com>
2022-03-13 20:55:20 +0200

    Upgrade Rails gem to 6.1.5 version

--- app/models/team_module.rb
+++ app/models/team_module.rb
@@ -2,5 +2,5 @@ class TeamModule < ApplicationRecord
   has_many :slots, dependent: :destroy
   has_many :lineups, dependent: :destroy
 
-  validates :name, presence: true, uniqueness: true, case_sensitive: false
+  validates :name, presence: true, uniqueness: { case_sensitive: false }
 end
