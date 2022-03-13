4ae411b82c752528258627adaf3802a57784a0c6
Bohdan Chaban <b.chaban.91@gmail.com>
2022-03-13 21:21:19 +0200

    Update migration

--- db/schema.rb
+++ db/schema.rb
@@ -2,11 +2,11 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# Note that this schema.rb definition is the authoritative source for your
-# database schema. If you need to create the application database on another
-# system, you should be using db:schema:load, not running all the migrations
-# from scratch. The latter is a flawed and unsustainable approach (the more migrations
-# you'll amass, the slower it'll run and the greater likelihood for issues).
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
@@ -406,4 +406,33 @@ ActiveRecord::Schema.define(version: 2022_02_06_094948) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "auction_bids", "auction_rounds"
+  add_foreign_key "auction_bids", "teams"
+  add_foreign_key "auction_rounds", "auctions"
+  add_foreign_key "auctions", "leagues"
+  add_foreign_key "clubs", "tournaments"
+  add_foreign_key "leagues", "seasons"
+  add_foreign_key "links", "tournaments"
+  add_foreign_key "match_players", "round_players"
+  add_foreign_key "national_matches", "tournament_rounds"
+  add_foreign_key "national_teams", "tournaments"
+  add_foreign_key "player_bids", "auction_bids"
+  add_foreign_key "player_bids", "players"
+  add_foreign_key "player_teams", "players"
+  add_foreign_key "player_teams", "teams"
+  add_foreign_key "players", "national_teams"
+  add_foreign_key "results", "leagues"
+  add_foreign_key "round_players", "players"
+  add_foreign_key "round_players", "tournament_rounds"
+  add_foreign_key "teams", "leagues"
+  add_foreign_key "teams", "users"
+  add_foreign_key "tournament_matches", "tournament_rounds"
+  add_foreign_key "tournament_rounds", "seasons"
+  add_foreign_key "tournament_rounds", "tournaments"
+  add_foreign_key "tours", "leagues"
+  add_foreign_key "tours", "tournament_rounds"
+  add_foreign_key "transfers", "auctions"
+  add_foreign_key "transfers", "leagues"
+  add_foreign_key "transfers", "players"
+  add_foreign_key "transfers", "teams"
 end
