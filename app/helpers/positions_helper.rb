22b9e089473c08033229f38d05166aa1dc2b215e
Bohdan Chaban <b.chaban.91@gmail.com>
2022-03-28 14:26:10 +0300

    Add new module images

--- app/helpers/positions_helper.rb
+++ app/helpers/positions_helper.rb
@@ -6,4 +6,8 @@ module PositionsHelper
   def slot_position_name(position_name)
     current_user&.ital_pos_naming ? position_name : Slot::POS_MAPPING[position_name]
   end
+
+  def modules_path
+    current_user&.ital_pos_naming ? 'modules/ital/' : 'modules/classic/'
+  end
 end
