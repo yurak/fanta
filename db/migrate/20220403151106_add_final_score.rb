71f06f13d7db0c2f0355a7e061adf8cde90921ba
Bohdan Chaban <b.chaban.91@gmail.com>
2022-04-16 17:56:22 +0300

    Remove service calls from migration

--- db/migrate/20220403151106_add_final_score.rb
+++ db/migrate/20220403151106_add_final_score.rb
@@ -2,12 +2,5 @@ class AddFinalScore < ActiveRecord::Migration[6.1]
   def change
     add_column :round_players, :final_score, :decimal,precision: 4, scale: 2, default: 0
     add_column :lineups, :final_score, :decimal,precision: 4, scale: 2, default: 0
-
-    RoundPlayer.where(score: 0).where.missing(:match_players).destroy_all
-    Tour.closed.each do |tour|
-      RoundPlayers::Updater.call(tour.tournament_round)
-
-      Lineups::Updater.call(tour)
-    end
   end
 end
