c4eb3290bd0880b90574335a6edb3b7652c9fd83
Yura Kuzhiy <yurakuzhiy@gmail.com>
2022-07-11 20:16:42 +0300

    Schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2022_07_11_125555) do
 
   create_table "article_tags", force: :cascade do |t|
