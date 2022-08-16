3a3b3401da60ea84aa0c83888cf471a76a5758c2
Bohdan Chaban <b.chaban.91@gmail.com>
2022-08-16 14:15:10 +0300

    Add fotmob match specs

--- spec/services/scores/injectors/fotmob_match_spec.rb
+++ spec/services/scores/injectors/fotmob_match_spec.rb
@@ -17,14 +17,13 @@ RSpec.describe Scores::Injectors::FotmobMatch do
 
     # TODO: add test cases
     context 'with finished match' do
-      let(:match) { create(:tournament_match, source_match_id: '3610099') }
+      let(:match) { create(:tournament_match, source_match_id: '3919071') }
 
-      it 'is a pending example'
-      # it 'returns path name' do
-      #   VCR.use_cassette 'fotmob_match_finished' do
-      #     expect(injector.call).to be_nil
-      #   end
-      # end
+      it 'updates players and returns missed players data' do
+        VCR.use_cassette 'fotmob_match_finished' do
+          expect(injector.call).not_to be_nil
+        end
+      end
     end
 
     # TODO: add test cases
