02fa835aa1f5f6db6d6653bde5501443c4d57d23
Bohdan Chaban <b.chaban.91@gmail.com>
2022-11-09 12:11:13 +0200

    Update lineup score after each injection

--- app/controllers/national_teams_controller.rb
+++ app/controllers/national_teams_controller.rb
@@ -3,7 +3,7 @@ class NationalTeamsController < ApplicationController
 
   respond_to :html
 
-  helper_method :national_team
+  helper_method :national_team, :national_teams
 
   def show; end
 
@@ -11,5 +11,9 @@ class NationalTeamsController < ApplicationController
 
   def national_team
     @national_team ||= NationalTeam.find(params[:id])
+  end
+
+  def national_teams
+    @national_teams ||= national_team.tournament.national_teams.active.order(:name)
   end
 end
