700407ef9a8d678924d7adb89082950d97b53238
Bohdan Chaban <b.chaban.91@gmail.com>
2022-11-20 22:22:00 +0200

    Fix round page for national teams

--- app/services/audit/csv_writer.rb
+++ app/services/audit/csv_writer.rb
@@ -11,24 +11,32 @@ module Audit
     def call
       CSV.open('log/missed_players.csv', 'ab') do |writer|
         if tournament.national_teams.any?
-          host_players.each do |player_record|
-            writer << [DateTime.now, tournament.name, t_match.tournament_round.number, t_match.host_team.name, t_match.id, player_record]
-          end
-          guest_players.each do |player_record|
-            writer << [DateTime.now, tournament.name, t_match.tournament_round.number, t_match.guest_team.name, t_match.id, player_record]
-          end
+          write_national_data(writer)
         else
-          host_players.each do |player_record|
-            writer << [DateTime.now, tournament.name, t_match.tournament_round.number, t_match.host_club.name, t_match.id, player_record]
-          end
-          guest_players.each do |player_record|
-            writer << [DateTime.now, tournament.name, t_match.tournament_round.number, t_match.guest_club.name, t_match.id, player_record]
-          end
+          write_club_data(writer)
         end
       end
     end
 
     private
+
+    def write_club_data(writer)
+      host_players.each do |player_record|
+        writer << [DateTime.now, tournament.name, t_match.tournament_round.number, t_match.host_club.name, t_match.id, player_record]
+      end
+      guest_players.each do |player_record|
+        writer << [DateTime.now, tournament.name, t_match.tournament_round.number, t_match.guest_club.name, t_match.id, player_record]
+      end
+    end
+
+    def write_national_data(writer)
+      host_players.each do |player_record|
+        writer << [DateTime.now, tournament.name, t_match.tournament_round.number, t_match.host_team.name, t_match.id, player_record]
+      end
+      guest_players.each do |player_record|
+        writer << [DateTime.now, tournament.name, t_match.tournament_round.number, t_match.guest_team.name, t_match.id, player_record]
+      end
+    end
 
     def tournament
       @tournament ||= t_match.tournament_round.tournament
