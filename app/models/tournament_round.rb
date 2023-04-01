432436507952c86dbaf047f6e52d0060849595ce
Bohdan Chaban <b.chaban.91@gmail.com>
2023-04-01 11:23:13 +0300

    Updated tour deadline countdown

--- app/models/tournament_round.rb
+++ app/models/tournament_round.rb
@@ -29,6 +29,6 @@ class TournamentRound < ApplicationRecord
     return '' unless deadline
 
     # TODO: remove '+ 2.hours' and implement timezones
-    TimeDifference.between(Time.zone.parse(deadline&.strftime('%a, %b %e at %H:%M')), 2.hours.from_now).in_general
+    TimeDifference.between(Time.zone.parse(deadline&.strftime('%a, %b %e at %H:%M')), 3.hours.from_now).in_general
   end
 end
