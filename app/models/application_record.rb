96f5704371a406a03f575251dea792015c599f38
Bohdan Chaban <b.chaban.91@gmail.com>
2023-07-25 11:02:23 +0300

    update url regexp

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -2,5 +2,5 @@ class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
 
   CONTENT_TYPE_PNG = 'image/png'.freeze
-  URL_REGEXP = %r{https?://(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&/=]*)}ix.freeze
+  URL_REGEXP = %r{https?://(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&/=]*)}ix.freeze
 end
