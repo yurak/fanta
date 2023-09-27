ea4f8458a0dc8b844bc8a02aebde3ae3d64f798a
Bohdan Chaban <b.chaban.91@gmail.com>
2023-09-27 17:08:55 +0300

    Fix time_to_deadline count

--- app/helpers/tours_helper.rb
+++ app/helpers/tours_helper.rb
@@ -3,7 +3,7 @@ module ToursHelper
     return '' if time_hash.blank?
 
     time_str = ''
-    time_str += "#{(time_hash[:weeks] * 7) + time_hash[:days]}d " if time_hash[:days]&.positive? || time_hash[:weeks]&.positive?
+    time_str += "#{(time_hash[:weeks].to_i * 7) + time_hash[:days].to_i}d " if time_hash[:days]&.positive? || time_hash[:weeks]&.positive?
     time_str += "#{time_hash[:hours]}h " if time_hash[:hours]&.positive?
     time_str += "#{time_hash[:minutes]}m " if time_hash[:minutes]&.positive?
     "#{time_str}left"
