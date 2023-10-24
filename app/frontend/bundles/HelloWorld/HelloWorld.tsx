435324daba20f4d33771e1a3a45af019f975bb6a
Yurii Oliiarnyk <oliiarnyk14@gmail.com>
2023-10-25 00:32:45 +0300

    Setup i18n and react-i18n

--- app/frontend/bundles/HelloWorld/HelloWorld.tsx
+++ app/frontend/bundles/HelloWorld/HelloWorld.tsx
@@ -1,11 +1,32 @@
 import { useState, useEffect } from "react";
+import { initReactI18next, useTranslation } from "react-i18next";
+import i18n from "i18next";
+import locales from "../../locales.json";
 import style from "./HelloWorld.module.scss";
+
+i18n.use(initReactI18next).init({
+  resources: {
+    en: {
+      translation: locales.en,
+    },
+    ua: {
+      translation: locales.au,
+    },
+  },
+  lng: "en", // if you're using a language detector, do not define the lng option
+  fallbackLng: "en",
+  interpolation: {
+    escapeValue: false,
+  },
+});
 
 interface IProps {
   name: string;
 }
 
 const HelloWorld = (props: IProps) => {
+  const { t } = useTranslation();
+
   const [name, setName] = useState(props.name);
   const [isClient, setIsClient] = useState(false);
 
@@ -18,6 +39,7 @@ const HelloWorld = (props: IProps) => {
       <h3>Hello, {name}!</h3>
       <hr />
       <h1>{isClient ? "This is never prerendered" : "Prerendered"}</h1>
+      <p>Translations are ready: {t("header.bid_made")}</p>
       <form>
         <label className={style.bright} htmlFor="name">
           Say hello to:
