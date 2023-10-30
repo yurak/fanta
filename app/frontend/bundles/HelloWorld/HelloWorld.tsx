f2454f12a61d0d9f3c564e2dee76d3e3b8483ad4
Yurii Oliiarnyk <oliiarnyk14@gmail.com>
2023-10-30 23:51:22 +0200

    Configurate locales

--- app/frontend/bundles/HelloWorld/HelloWorld.tsx
+++ app/frontend/bundles/HelloWorld/HelloWorld.tsx
@@ -1,44 +1,21 @@
-import { useState, useEffect } from "react";
-import { initReactI18next, useTranslation } from "react-i18next";
-import i18n from "i18next";
-import locales from "../../locales.json";
+import { useState } from "react";
+import { useTranslation } from "react-i18next";
+import { AppProvider } from "../../providers/AppProvider";
 import style from "./HelloWorld.module.scss";
-
-i18n.use(initReactI18next).init({
-  resources: {
-    en: {
-      translation: locales.en,
-    },
-    ua: {
-      translation: locales.au,
-    },
-  },
-  lng: "en", // if you're using a language detector, do not define the lng option
-  fallbackLng: "en",
-  interpolation: {
-    escapeValue: false,
-  },
-});
 
 interface IProps {
   name: string;
 }
 
-const HelloWorld = (props: IProps) => {
+const HelloWorld = ({ name: defaultName }: IProps) => {
   const { t } = useTranslation();
 
-  const [name, setName] = useState(props.name);
-  const [isClient, setIsClient] = useState(false);
-
-  useEffect(() => {
-    setIsClient(true);
-  }, []);
+  const [name, setName] = useState(defaultName);
 
   return (
     <div>
       <h3>Hello, {name}!</h3>
       <hr />
-      <h1>{isClient ? "This is never prerendered" : "Prerendered"}</h1>
       <p>Translations are ready: {t("header.bid_made")}</p>
       <form>
         <label className={style.bright} htmlFor="name">
@@ -50,4 +27,8 @@ const HelloWorld = (props: IProps) => {
   );
 };
 
-export default HelloWorld;
+export default (props: IProps) => (
+  <AppProvider>
+    <HelloWorld {...props} />
+  </AppProvider>
+);
