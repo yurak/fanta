2452f9be36820602435b41979bff635d66fe6770
Yurii Oliiarnyk <oliiarnyk14@gmail.com>
2023-10-31 20:12:22 +0200

    Rename packs to pages

--- client/packs/server-bundle.ts
+++ client/packs/server-bundle.ts
@@ -1,6 +1,6 @@
 import ReactOnRails from "react-on-rails";
 
-import HelloWorld from "../bundles/HelloWorld/HelloWorldServer";
+import HelloWorld from "../pages/HelloWorld/HelloWorldServer";
 
 // This is how react_on_rails can see the HelloWorld in the browser.
 ReactOnRails.register({
