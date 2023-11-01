e6cdd994fc9d51e8cbe656e6c398873817969c23
Yurii Oliiarnyk <oliiarnyk14@gmail.com>
2023-11-01 10:57:10 +0200

    Connected client script to app layout

--- app/client/entry/client-bundle.ts
+++ app/client/entry/client-bundle.ts
@@ -2,7 +2,6 @@ import ReactOnRails from "react-on-rails";
 
 import HelloWorld from "../pages/HelloWorld";
 
-// This is how react_on_rails can see the HelloWorld in the browser.
 ReactOnRails.register({
   HelloWorld,
 });
