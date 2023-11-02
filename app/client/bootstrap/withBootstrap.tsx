225a97164a63f763f16dd00d14479f6b0f509d03
Bohdan Chaban <b.chaban.91@gmail.com>
2023-11-02 12:58:09 +0200

    Add react import

--- app/client/bootstrap/withBootstrap.tsx
+++ app/client/bootstrap/withBootstrap.tsx
@@ -1,6 +1,7 @@
 import { useSSR } from "react-i18next";
 import { resources } from "../locales/resources";
 import { AppProvider } from "./AppProvider";
+import React from "react";
 
 export const withBootstrap = (Component: React.ElementType) => (props, railsContext) => {
   return () => {
