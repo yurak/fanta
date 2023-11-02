2146905d23bc622f658d6412eab8c74640531b92
Yurii Oliiarnyk <oliiarnyk14@gmail.com>
2023-11-02 13:34:11 +0200

    Fixed ts types

--- app/client/bootstrap/withBootstrap.tsx
+++ app/client/bootstrap/withBootstrap.tsx
@@ -1,11 +1,14 @@
 import React from "react";
-import { RailsContext } from "react-on-rails/node_package/lib/types";
+import {
+  RailsContext,
+  ReactComponentOrRenderFunction,
+} from "react-on-rails/node_package/lib/types";
 import { useSSR } from "react-i18next";
 import { resources } from "../locales/resources";
 import { AppProvider } from "./AppProvider";
 
 export const withBootstrap =
-  <T extends {}>(Component: React.ComponentType<T>) =>
+  <T extends {}>(Component: React.ComponentType<T>): ReactComponentOrRenderFunction =>
   (props: T, railsContext: RailsContext) => {
     return () => {
       useSSR(resources, railsContext.i18nLocale);
