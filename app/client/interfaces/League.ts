6b59824cad4bf3048644af8e329c014f92246590
Yurii Oliiarnyk <oliiarnyk14@gmail.com>
2023-11-10 01:22:00 +0200

    WIP: leagues template

---
+++ app/client/interfaces/League.ts
@@ -0,0 +1,18 @@
+export interface ILeague {
+  auction_type: string;
+  cloning_status: string;
+  created_at: string;
+  division_id: number;
+  id: number;
+  max_avg_def_score: string;
+  min_avg_def_score: string;
+  name: string;
+  promotion: number;
+  relegation: number;
+  season_id: number;
+  status: string;
+  tour_difference: number;
+  tournament_id: number;
+  transfer_status: string;
+  updated_at: string;
+}
