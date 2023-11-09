dfeb7b706487ff0001c3db55c29e0d7b647ca110
Yurii Oliiarnyk <oliiarnyk14@gmail.com>
2023-11-10 01:00:50 +0200

    Created select component

---
+++ app/client/ui/Select/Select.tsx
@@ -0,0 +1,63 @@
+import React from "react";
+import ReactSelect, { ActionMeta, OnChangeValue, PropsValue, components } from "react-select";
+import arrowDown from "../../../assets/images/icons/arrow_down.svg";
+import { useIsClient } from "../../hooks/useIsClient";
+import styles from "./Select.module.scss";
+
+const Select = <Option extends unknown, IsMulti extends boolean = false>({
+  value,
+  onChange,
+  options,
+  placeholder,
+  icon,
+}: {
+  options: Option[];
+  value: PropsValue<Option>;
+  onChange: (newValue: OnChangeValue<Option, IsMulti>, actionMeta: ActionMeta<Option>) => void;
+  placeholder?: string;
+  icon?: React.ReactNode;
+}) => {
+  // TODO: temporary solution, need to fix working with ssr
+  const isClient = useIsClient();
+  if (!isClient) {
+    return null;
+  }
+
+  return (
+    <ReactSelect
+      styles={{
+        control: (styles) => ({
+          ...styles,
+          minHeight: 40,
+        }),
+        valueContainer: (styles) => ({
+          ...styles,
+          ...(icon && {
+            paddingLeft: 5,
+          }),
+        }),
+      }}
+      options={options}
+      isSearchable={false}
+      placeholder={placeholder}
+      onChange={onChange}
+      value={value}
+      components={{
+        IndicatorSeparator: null,
+        DropdownIndicator: () => (
+          <span className={styles.dropdownIndicator}>
+            <img src={arrowDown} />
+          </span>
+        ),
+        ValueContainer: (props) => (
+          <>
+            {icon && <span className={styles.valueIcon}>{icon}</span>}
+            {components.ValueContainer(props)}
+          </>
+        ),
+      }}
+    />
+  );
+};
+
+export default Select;
