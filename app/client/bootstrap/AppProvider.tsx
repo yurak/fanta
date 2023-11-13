527a3ddc120eaef292227e8e3e160264e49fe7e8
Yurii Oliiarnyk <oliiarnyk14@gmail.com>
2023-11-13 13:56:07 +0200

    Init react query

--- app/client/bootstrap/AppProvider.tsx
+++ app/client/bootstrap/AppProvider.tsx
@@ -1,7 +1,16 @@
 import React from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { initReactI18next } from "react-i18next";
 import i18n from "i18next";
 import { resources } from "../locales/resources";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 10 * 60 * 1000, // 10min
+    },
+  },
+});
 
 i18n.use(initReactI18next).init({
   resources,
@@ -16,4 +25,6 @@ interface IProps {
   children: React.ReactNode;
 }
 
-export const AppProvider = ({ children }: IProps) => <>{children}</>;
+export const AppProvider = ({ children }: IProps) => (
+  <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+);
