8c452477e4722072b627725456a3e2c8550ba66f
Yurii Oliiarnyk <oliiarnyk14@gmail.com>
2023-11-14 22:25:08 +0200

    Move page heading to components folder

---
+++ app/client/components/PageHeading/PageHeading.tsx
@@ -0,0 +1,10 @@
+import styles from "./PageHeading.module.scss";
+
+const PageHeading = ({ title, description }: { title: string; description?: string }) => (
+  <div>
+    <h3 className={styles.title}>{title}</h3>
+    {description && <p className={styles.description}>{description}</p>}
+  </div>
+);
+
+export default PageHeading;
