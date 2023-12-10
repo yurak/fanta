933013ee31eb47478ef4f6233c913295e0f6b52a
Bohdan Chaban <bohdan.chaban@itv.com>
2023-12-10 17:33:55 +0200

    Add divisions API endpoint (#407)

---
+++ app/serializers/division_level_serializer.rb
@@ -0,0 +1,13 @@
+class DivisionLevelSerializer < ActiveModel::Serializer
+  attributes :id
+  attributes :level
+  attributes :leagues
+
+  def leagues
+    League.includes(:division).joins(:division)
+          .by_season(instance_options[:season_id])
+          .by_tournament(instance_options[:tournament_id])
+          .where(divisions: { level: object.level })
+          .map { |l| LeagueBaseSerializer.new(l) }
+  end
+end
