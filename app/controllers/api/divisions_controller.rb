027099e9c9d1ef47791f8224931b989896cd72e7
Bohdan Chaban <b.chaban.91@gmail.com>
2023-12-10 17:01:11 +0200

    Add divisions API endpoint

---
+++ app/controllers/api/divisions_controller.rb
@@ -0,0 +1,32 @@
+module Api
+  class DivisionsController < ApplicationController
+    skip_before_action :authenticate_user!, only: %i[index]
+
+    respond_to :json
+
+    def index
+      if tournament
+        divisions = division_levels
+                      .map { |t| DivisionLevelSerializer.new(t, tournament_id: tournament.id, season_id: season.id ) }
+        render json: { data: divisions }
+      else
+        not_found
+      end
+    end
+
+    private
+
+    def division_levels
+      Division.includes(:leagues).joins(:leagues)
+              .where('leagues.season_id = ? AND leagues.tournament_id = ?', season.id, tournament.id).uniq(&:level)
+    end
+
+    def tournament
+      @tournament ||= Tournament.find_by(id: params[:tournament_id])
+    end
+
+    def season
+      @season ||= Season.find_by(id: params[:season_id]) || Season.last
+    end
+  end
+end
---
+++ spec/requests/api/divisions_spec.rb
@@ -0,0 +1,41 @@
+require 'swagger_helper'
+
+RSpec.describe 'Divisions' do
+  path '/api/tournaments/{tournament_id}/divisions' do
+    parameter name: 'tournament_id', in: :path, type: :string, description: 'Tournament id'
+    parameter name: 'season_id', in: :query, type: :string, required: false, description: 'Season id'
+
+    get('list tournament divisions') do
+      tags 'Divisions'
+      consumes 'application/json'
+      produces 'application/json'
+
+      response 200, 'Success' do
+        let!(:tournament) { create(:tournament) }
+        let!(:division) { create(:division, level: 'A') }
+        let!(:league) { create(:league, tournament: tournament, division: division) }
+        let!(:tournament_id) { tournament.id }
+
+        schema type: :object,
+               properties: {
+                 data: { type: :array, items: { '$ref' => '#/components/schemas/division' } }
+               }
+
+        run_test! do |response|
+          body = JSON.parse(response.body)
+
+          expect(body['data'].first['id']).to eq division.id
+          expect(body['data'].first['leagues'].first['id']).to eq league.id
+        end
+      end
+
+      response 404, 'Not found' do
+        let!(:tournament_id) { 'invalid' }
+
+        schema '$ref' => '#/components/schemas/error_not_found'
+
+        run_test!
+      end
+    end
+  end
+end
