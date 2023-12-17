318aa273def6dc942cf44a4a5e2037b41836928b
Bohdan Chaban <bohdan.chaban@itv.com>
2023-12-17 12:05:40 +0200

    Add order to players API endpoints (#410)

--- app/services/players/search.rb
+++ app/services/players/search.rb
@@ -1,10 +1,21 @@
 module Players
   class Search < ApplicationService
-    attr_reader :club_id, :league_id, :name, :position, :tournament_id
+    attr_reader :club_id, :league_id, :max_app, :max_base_score, :max_total_score, :min_app, :min_base_score, :min_total_score,
+                :name, :position, :tournament_id
+
+    MAX_SEARCH = 100
+    MIN_SEARCH = -100
+    ZERO_SEARCH = 0
 
     def initialize(params)
       @club_id = params[:club_id]
       @league_id = params[:league_id]
+      @max_app = params[:max_app]
+      @max_base_score = params[:max_base_score]
+      @max_total_score = params[:max_total_score]
+      @min_app = params[:min_app]
+      @min_base_score = params[:min_base_score]
+      @min_total_score = params[:min_total_score]
       @name = params[:name]
       @position = params[:position]
       @tournament_id = params[:tournament_id]
@@ -12,22 +23,59 @@ module Players
 
     def call
       players = Player.by_tournament(tournament)
-                      .by_club(club_id)
+                      .by_club(club_ids)
                       .by_classic_position(position)
-      players = Player.where(id: players.pluck(:id).uniq).search_by_name(name) if name
+
+      players = search_by_name(players) if name
+      players = search_by_appearances(players, min_app, max_app) if min_app || max_app
+      players = search_by_base_score(players, min_base_score, max_base_score) if min_base_score || max_base_score
+      players = search_by_total_score(players, min_total_score, max_total_score) if min_total_score || max_total_score
       players
     end
 
     private
 
-    def tournament
-      tournament_id = league ? league.tournament_id : @tournament_id
+    def search_by_name(players)
+      Player.where(id: players.pluck(:id).uniq).search_by_name(name)
+    end
 
-      Tournament.find_by(id: tournament_id)
+    def search_by_appearances(players, min_value, max_value)
+      max = max_value ? max_value.to_i : MAX_SEARCH
+      min = min_value ? min_value.to_i : ZERO_SEARCH
+
+      Player.where(id: players.select { |pl| pl.season_scores_count >= min && pl.season_scores_count <= max })
+    end
+
+    def search_by_base_score(players, min_value, max_value)
+      max = max_value ? max_value.to_f : MAX_SEARCH
+      min = min_value ? min_value.to_f : MIN_SEARCH
+
+      Player.where(id: players.select { |pl| pl.season_average_score >= min && pl.season_average_score <= max })
+    end
+
+    def search_by_total_score(players, min_value, max_value)
+      max = max_value ? max_value.to_f : MAX_SEARCH
+      min = min_value ? min_value.to_f : MIN_SEARCH
+
+      Player.where(id: players.select { |pl| pl.season_average_result_score >= min && pl.season_average_result_score <= max })
+    end
+
+    def club_ids
+      return club_id if club_id.present?
+
+      Club.active.by_tournament(tournament_ids).pluck(:id) if tournament_ids
+    end
+
+    def tournament_ids
+      league ? league.tournament_id : @tournament_id
+    end
+
+    def tournament
+      Tournament.find_by(id: league.tournament_id) if league
     end
 
     def league
-      League.find_by(id: league_id)
+      @league ||= League.find_by(id: league_id)
     end
   end
 end
---
+++ spec/services/players/search_spec.rb
@@ -0,0 +1,5 @@
+RSpec.describe Players::Search do
+  describe '#call' do
+    it 'is a pending example'
+  end
+end
