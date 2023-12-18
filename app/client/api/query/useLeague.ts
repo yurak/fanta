1de0c128b89a7d29f3f892d8b41a32a8bf19c52e
Yurii Oliiarnyk <oliiarnyk14@gmail.com>
2023-12-18 20:53:44 +0200

    Fixed cache time

--- app/client/api/query/useLeague.ts
+++ app/client/api/query/useLeague.ts
@@ -3,9 +3,9 @@ import { useQuery } from "@tanstack/react-query";
 import { IResponse } from "../../interfaces/api/Response";
 import { ILeagueFullData } from "../../interfaces/League";
 
-export const useLeague = (id: number, enabled: boolean) => {
+export const useLeague = (id: number, enabled?: boolean) => {
   const query = useQuery({
-    staleTime: 1000 * 60 * 10, // 30 minutes
+    staleTime: 1000 * 60 * 30, // 30 minutes
     queryKey: ["league", id],
     queryFn: async ({ signal }) => {
       return (await axios.get<IResponse<ILeagueFullData>>(`/leagues/${id}`, { signal })).data.data;
