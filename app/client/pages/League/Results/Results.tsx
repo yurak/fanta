ca1a06824cdf3e54bbce3d1f9277e210c91feb9e
Yurii Oliiarnyk <oliiarnyk14@gmail.com>
2024-02-02 16:26:31 +0200

    Replaced division link

--- app/client/pages/League/Results/Results.tsx
+++ app/client/pages/League/Results/Results.tsx
@@ -1,12 +1,12 @@
 import { useTranslation } from "react-i18next";
 import { useParams } from "react-router-dom";
+import Link from "../../../ui/Link";
 import { useLeague } from "../../../api/query/useLeague";
 import PageHeading from "../../../components/Heading";
 import PageLayout from "../../../layouts/PageLayout";
 import ResultsMantra from "./ResultsMantra";
 import ResultsFanta from "./ResultsFanta";
 import ResultsSkeleton from "./ResultsSkeleton";
-import ArrowRight from "../../../assets/icons/arrow-right.svg";
 import styles from "./Results.module.scss";
 
 const ResultsPage = () => {
@@ -20,13 +20,9 @@ const ResultsPage = () => {
       <div className={styles.heading}>
         <PageHeading title={t("table.title")} noSpace />
         {league.data?.mantra_format && (
-          <a
-            className={styles.divisionsLink}
-            href={`/tournaments/${league.data?.tournament_id}/divisions`}
-          >
-            📊 {t("divisions.title")}
-            <ArrowRight className={styles.divisionsLinkIcon} />
-          </a>
+          <Link to={`/tournaments/${league.data?.tournament_id}/divisions`}>
+            {t("table.division")} {league.data?.division}
+          </Link>
         )}
       </div>
       {league.data ? (
