20b747b89963ddc82b0e7582e371f76fb21618ad
Bohdan Chaban <b.chaban.91@gmail.com>
2024-02-09 00:22:45 +0200

    Update fanta lineup limits

--- spec/services/results/national_updater_spec.rb
+++ spec/services/results/national_updater_spec.rb
@@ -23,22 +23,29 @@ RSpec.describe Results::NationalUpdater do
     context 'when tour with lineups' do
       let(:team_one) { create(:team, :with_result) }
       let(:team_two) { create(:team, :with_result) }
+      let(:team_forty) { create(:team, :with_result) }
 
       before do
         create(:lineup, :with_team_and_score_six, team: team_one, tour: tour)
         create(:lineup, :with_team_and_score_seven, team: team_two, tour: tour)
+        create_list(:lineup, 37, team: create(:team, :with_result), tour: tour, final_score: 62)
+        create(:lineup, :with_team_and_score_five, team: team_forty, tour: tour)
 
         updater.call
       end
 
       it { expect(team_one.results.last.total_score).to eq(67) }
-      it { expect(team_one.results.last.points).to eq(18) }
+      it { expect(team_one.results.last.points).to eq(54) }
       it { expect(team_one.results.last.best_lineup).to eq(67) }
       it { expect(team_one.results.last.draws).to eq(1) }
       it { expect(team_two.results.last.total_score).to eq(82) }
-      it { expect(team_two.results.last.points).to eq(25) }
+      it { expect(team_two.results.last.points).to eq(60) }
       it { expect(team_two.results.last.best_lineup).to eq(82) }
       it { expect(team_two.results.last.draws).to eq(1) }
+      it { expect(team_forty.results.last.total_score).to eq(55) }
+      it { expect(team_forty.results.last.points).to eq(1) }
+      it { expect(team_forty.results.last.best_lineup).to eq(55) }
+      it { expect(team_forty.results.last.draws).to eq(1) }
     end
   end
 end
