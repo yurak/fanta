709d3132ced543e9941c96bef6ed2a840056c58c
Yurii Oliiarnyk <oliiarnyk14@gmail.com>
2024-02-16 22:59:34 +0200

    WIP: Added some columns to players list

--- app/client/pages/Players/PlayersList/PlayersList.tsx
+++ app/client/pages/Players/PlayersList/PlayersList.tsx
@@ -1,4 +1,6 @@
+import PlayerAvatar from "@/components/PlayerAvatar";
 import { IPlayer } from "@/interfaces/Player";
+import Table from "@/ui/Table";
 
 const PlayersList = ({
   items,
@@ -13,13 +15,47 @@ const PlayersList = ({
 }) => {
   return (
     <div>
-      <div>
-        {items.map((item) => (
-          <div key={item.id}>
-            {item.first_name} {item.name}
-          </div>
-        ))}
-      </div>
+      <Table
+        dataSource={items}
+        columns={[
+          {
+            dataKey: "name",
+            title: "Name",
+            render: ({ first_name, name, avatar_path, club }) => (
+              <div>
+                <div>
+                  <PlayerAvatar avatarSrc={avatar_path} clubKitSrc={club.kit_path} />
+                </div>
+                <div>
+                  <div>{first_name}</div>
+                  <div>{name}</div>
+                </div>
+              </div>
+            ),
+          },
+          {
+            dataKey: "average_price",
+            title: "Price",
+            render: ({ average_price }) => (average_price ? `${average_price}M` : "-"),
+          },
+          {
+            dataKey: "teams_count",
+            title: "# of teams",
+          },
+          {
+            dataKey: "appearances",
+            title: "Apps",
+          },
+          {
+            dataKey: "average_base_score",
+            title: "BS",
+          },
+          {
+            dataKey: "average_total_score",
+            title: "TS",
+          },
+        ]}
+      />
       <div>
         <button onClick={() => fetchNextPage()} disabled={!hasNextPage || isFetchingNextPage}>
           {isFetchingNextPage
