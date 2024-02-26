98df6cdd9a9ea9005443d4eff257805ff6026b7d
Yurii Oliiarnyk <oliiarnyk14@gmail.com>
2024-02-26 11:30:39 +0200

    Format numbers

--- app/client/helpers/formatNumber.ts
+++ app/client/helpers/formatNumber.ts
@@ -1,3 +1,19 @@
-export const formatNumber = (value: number) => {
-  return Intl.NumberFormat().format(value);
+import React from "react";
+
+interface IOptions extends Intl.NumberFormatOptions {
+  zeroFallback?: React.ReactNode,
+  suffix?: string,
+}
+
+export const formatNumber = (
+  value: number,
+  { zeroFallback, suffix, ...options }: IOptions = {}
+) => {
+  if (value === 0) {
+    return zeroFallback ?? 0;
+  }
+
+  return `${Intl.NumberFormat("en", {
+    ...options,
+  }).format(value)}${suffix ?? ""}`;
 };
