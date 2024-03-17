f71dd322cfe45efe1a632ff6b545d9799b364b2c
Bohdan Chaban <b.chaban.91@gmail.com>
2024-03-17 15:14:44 +0200

    add session_store

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -109,4 +109,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.telegram_updates_controller.session_store = :file_store, Rails.root.join('tmp', 'session_store')
 end
