6d5299bd0c41119e9b29e9cd59bc096cffbb43c6
Yurii Oliiarnyk <oliiarnyk14@gmail.com>
2024-03-22 17:30:36 +0200

    Create popover input component

--- app/client/pages/Players/PlayersFilters/PlayersFilters.tsx
+++ app/client/pages/Players/PlayersFilters/PlayersFilters.tsx
@@ -1,20 +1,57 @@
-import Popover from "@/ui/Popover";
+import { useState } from "react";
+import PopoverInput from "@/ui/PopoverInput";
 
 const PlayerFilters = () => {
+  const [position, setPosition] = useState<string[]>([]);
+  const [appearences, setAppearences] = useState<string | null>("5 to 10");
+  const [totalScore, setTotalScore] = useState<string | null>("2 - 9");
+
   return (
     <>
       All Filters
       <br />
-      <Popover
-        title="Position"
-        renderedReference={({ isOpen, setRef, ...rest }) => (
-          <button ref={setRef} disabled={isOpen} {...rest}>
-            Reference
-          </button>
-        )}
-      >
-        Positions
-      </Popover>
+      <PopoverInput
+        label="Position"
+        value={position}
+        clearValue={() => setPosition([])}
+        formatValue={(value) => (value.length > 0 ? value.toString() : null)}
+      />
+      <PopoverInput
+        label="Appeareances"
+        value={appearences}
+        clearValue={() => setAppearences(null)}
+        formatValue={(value) => (value ? `From ${value}` : null)}
+      />
+      <PopoverInput
+        label="Total score"
+        value={totalScore}
+        clearValue={() => setTotalScore(null)}
+        formatValue={(value) => (value ? `TS: ${value}` : null)}
+      />
+      <PopoverInput
+        label="Total score"
+        value={totalScore}
+        clearValue={() => setTotalScore(null)}
+        formatValue={(value) => (value ? `TS: ${value}` : null)}
+      />
+      <PopoverInput
+        label="Total score"
+        value={totalScore}
+        clearValue={() => setTotalScore(null)}
+        formatValue={(value) => (value ? `TS: ${value}` : null)}
+      />
+      <PopoverInput
+        label="Total score"
+        value={totalScore}
+        clearValue={() => setTotalScore(null)}
+        formatValue={(value) => (value ? `TS: ${value}` : null)}
+      />
+      <PopoverInput
+        label="Total score"
+        value={totalScore}
+        clearValue={() => setTotalScore(null)}
+        formatValue={(value) => (value ? `TS: ${value}` : null)}
+      />
     </>
   );
 };
