681b8ddbd9406336765d65386166d844fd3165d9
Bohdan Chaban <bohdan.chaban@itv.com>
2024-03-23 20:44:55 +0200

    Add open_join to tournaments (#433)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,6 +3,10 @@ module ApplicationHelper
     Tournament.with_clubs.order(:id) + Tournament.with_ec_clubs
   end
 
+  def open_tournaments
+    Tournament.open_join
+  end
+
   def position_number(index)
     return '🥇' if index == 1
     return '🥈' if index == 2
