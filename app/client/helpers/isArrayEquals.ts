390a7d1ed373b0cb7d2f8bee53103a1ad3afc547
Yurii Oliiarnyk <oliiarnyk14@gmail.com>
2024-03-28 23:39:18 +0200

    Code improvements

--- app/client/helpers/isArrayEquals.ts
+++
@@ -1,7 +0,0 @@
-export const isArrayEquals = (array1: number[], array2: number[]) => {
-  if (array1.length !== array2.length) {
-    return false;
-  }
-
-  return array1.every((value, index) => array2[index] === value);
-};
