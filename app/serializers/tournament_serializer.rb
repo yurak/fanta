7bfc06aa6828ffa506bbd6857fbc18d783e0851a
Bohdan Chaban <bohdan.chaban@itv.com>
2024-04-13 19:06:09 +0300

    Add clubs to tournament endpoint (#435)

--- app/serializers/tournament_serializer.rb
+++ app/serializers/tournament_serializer.rb
@@ -5,6 +5,14 @@ class TournamentSerializer < ActiveModel::Serializer
   attributes :mantra_format
   attributes :name
   attributes :short_name
+  attributes :clubs
+
+  def clubs
+    return unless instance_options[:clubs]
+
+    clubs = object.fanta? ? object.ec_clubs.active : object.clubs.active
+    clubs.map { |club| ClubSerializer.new(club) }
+  end
 
   def logo
     ActionController::Base.helpers.asset_path(object.logo_path)
