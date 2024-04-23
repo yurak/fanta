8351b3f528f16c933bea3051622199827fca9b20
Yurii Oliiarnyk <oliiarnyk14@gmail.com>
2024-04-23 11:19:17 +0300

    Add show/hide toggle state for drawer section

--- app/client/ui/Drawer/DrawerSection/DrawerSection.tsx
+++ app/client/ui/Drawer/DrawerSection/DrawerSection.tsx
@@ -1,10 +1,24 @@
+import { useState } from "react";
+import cn from "classnames";
+import ArrowDown from "@/assets/icons/arrow-down.svg";
 import styles from "./DrawerSection.module.scss";
 
 const DrawerSection = ({ title, children }: { title: string, children: React.ReactNode }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <div className={styles.section}>
-      <div className={styles.title}>{title}</div>
-      <div>{children}</div>
+      <button className={styles.title} onClick={() => setIsOpen((o) => !o)}>
+        {title}
+        <span
+          className={cn(styles.icon, {
+            [styles.isActive]: isOpen,
+          })}
+        >
+          <ArrowDown />
+        </span>
+      </button>
+      {isOpen && <div className={styles.content}>{children}</div>}
     </div>
   );
 };
