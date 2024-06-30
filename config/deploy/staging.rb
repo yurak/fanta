a53ff34c5b0bf0f259c824af3962bbf9ed717c14
Bohdan Chaban <b.chaban.91@gmail.com>
2024-06-30 23:01:36 +0300

    Add new clubs, fix CSP (#457)

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,8 +1,8 @@
 # cap staging deploy BRANCH=staging
 
-role :app, %w{ubuntu@34.253.224.14}
-role :web, %w{ubuntu@34.253.224.14}
-role :db, %w{ubuntu@34.253.224.14}
+role :app, %w{ubuntu@3.252.122.206}
+role :web, %w{ubuntu@3.252.122.206}
+role :db, %w{ubuntu@3.252.122.206}
 set :ssh_options, {
   keys: %w(~/.ssh/mantrakey.pem),
   forward_agent: false,
