0107af92a20ad3687641e0b0258333d853113615
Bohdan Chaban <b.chaban.91@gmail.com>
2024-06-30 22:27:35 +0300

    Add new clubs, fix CSP

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -4,17 +4,19 @@
 # For further information see the following documentation
 # https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy
 
-# Rails.application.config.content_security_policy do |policy|
+aws_urls = %w[*.amazonaws.com/player_avatars/ *.amazonaws.com/players/ *.amazonaws.com/kits/ *.amazonaws.com/club_logo/]
+
+Rails.application.config.content_security_policy do |policy|
 #   policy.default_src :self, :https
 #   policy.font_src    :self, :https, :data
 #   policy.img_src     :self, :https, :data
-#   policy.object_src  :none
+  policy.object_src(*aws_urls)
 #   policy.script_src  :self, :https
 #   policy.style_src   :self, :https
 
 #   # Specify URI for violation reports
 #   # policy.report_uri "/csp-violation-report-endpoint"
-# end
+end
 
 # If you are using UJS then enable automatic nonce generation
 # Rails.application.config.content_security_policy_nonce_generator = -> request { SecureRandom.base64(16) }
