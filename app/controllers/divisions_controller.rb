7b47b74c35f34bc1eb4459a2a76c988a7841a580
Bohdan Chaban <b.chaban.91@gmail.com>
2024-07-21 01:35:44 +0300

    Update divisions ui, update auction conditions

--- app/controllers/divisions_controller.rb
+++ app/controllers/divisions_controller.rb
@@ -4,8 +4,8 @@ class DivisionsController < ApplicationController
   respond_to :html
 
   def index
-    @leagues = League.includes(:division, results: :team).active.with_division.by_tournament(tournament.id)
-                     .uniq.sort_by { |x| [x.division.id] }
+    @leagues = League.joins(:division).by_season(season.id).by_tournament(tournament.id).order('divisions.level')
+                     .group_by { |league| league.division.level }
   end
 
   private
@@ -16,5 +16,9 @@ class DivisionsController < ApplicationController
 
   def tournament
     @tournament ||= Tournament.find_by(id: divisions_params[:tournament_id])
+  end
+
+  def season
+    @season ||= Season.last
   end
 end
