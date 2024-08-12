114ea3034c3ce0abcb750acd4adc24116e057864
Bohdan Chaban <b.chaban.91@gmail.com>
2024-08-13 00:18:50 +0300

    Update events processing

--- app/services/scores/injectors/sofascore.rb
+++ app/services/scores/injectors/sofascore.rb
@@ -4,7 +4,8 @@ module Scores
       private
 
       def inject_match_scores(tournament_match)
-        Scores::Injectors::SofascoreMatch.call(tournament_match)
+        # TODO: fix injector
+        # Scores::Injectors::SofascoreMatch.call(tournament_match)
       end
     end
   end
