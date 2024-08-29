6b217a2d9b140eb0a6bc2e9a7ea877ee1413261d
Yura Kuzhiy <yurakuzhiy@gmail.com>
2024-08-29 09:46:43 +0300

    Add subs string

--- app/helpers/players_helper.rb
+++ app/helpers/players_helper.rb
@@ -14,6 +14,11 @@ module PlayersHelper
     team.players.sort_by(&:position_sequence_number)
   end
 
+  def subs_string(match_player)
+    "Replaced: #{match_player.main_subs.last&.out_rp&.full_name_reverse} by #{match_player.main_subs&.first&.subs_by}"
+  end
+
+
   def available_by_slot(team, slot)
     return {} unless slot
 
