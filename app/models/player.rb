b174b8bb592de467ce12170e9c01e24e28d8d8b1
Bohdan Chaban <b.chaban.91@gmail.com>
2025-03-19 15:06:48 +0200

    Update user edit page, add locale to users (#489)

--- app/models/player.rb
+++ app/models/player.rb
@@ -49,6 +49,25 @@ class Player < ApplicationRecord
     xk: 'Kosovo'
   }.freeze
 
+  COUNTRY = {
+    bo: 'Bolivia',
+    cd: 'DR Congo',
+    'gb-eng': 'England',
+    'gb-wls': 'Wales',
+    'gb-sct': 'Scotland',
+    'gb-nir': 'Northern Ireland',
+    ir: 'Iran',
+    kr: 'Korea',
+    md: 'Moldova',
+    ps: 'Palestine',
+    ru: 'terrorist state',
+    sy: 'Syria',
+    tz: 'Tanzania',
+    us: 'USA',
+    ve: 'Venezuela',
+    xk: 'Kosovo'
+  }.freeze
+
   scope :by_club, ->(club_id) { where(club_id: club_id) if club_id.present? }
   scope :search_by_name, lambda { |search_str|
     where('lower(players.name) LIKE :search OR lower(players.first_name) LIKE :search', search: "%#{search_str}%")
