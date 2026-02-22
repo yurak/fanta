732ec856f6f8225517ee3840d53c33298834cf19
Bohdan Chaban <b.chaban.91@gmail.com>
2026-02-22 22:43:07 +0200

    Fix specs for rubocop

--- spec/services/telegram_bot/tour/opened_notifier_spec.rb
+++ spec/services/telegram_bot/tour/opened_notifier_spec.rb
@@ -4,24 +4,40 @@ require 'rails_helper'
 
 RSpec.describe TelegramBot::Tour::OpenedNotifier do
   def build_notifiable(league: nil)
-    instance_double('Tour', league: league, number: 1, tournament_round: instance_double('TournamentRound', deadline: Time.current))
+    instance_double(Tour, league: league, number: 1, tournament_round: instance_double(TournamentRound, deadline: Time.current))
   end
 
   def build_league
-    tournament = instance_double('Tournament', icon: '🏆', code: 'T')
-    instance_double('League', name: 'League', tournament: tournament)
+    tournament = instance_double(Tournament, icon: '🏆', code: 'T')
+    instance_double(League, name: 'League', tournament: tournament)
   end
 
   def build_user
-    instance_double('User', locale: 'en', time_zone: 'UTC', local_time: 'Fri, Feb 7, 12:00')
+    instance_double(User, locale: 'en', time_zone: 'UTC', local_time: 'Fri, Feb 7, 12:00')
   end
 
   def build_team(user:)
-    instance_double('Team', user: user, human_name: 'Team')
+    instance_double(Team, user: user, human_name: 'Team')
   end
 
   def build_notification(team:, notifiable:)
-    instance_double('Notification', team: team, notifiable: notifiable)
+    instance_double(Notification, team: team, notifiable: notifiable)
+  end
+
+  def build_valid_notification
+    league = build_league
+    tour   = build_notifiable(league: league)
+    user   = build_user
+    team   = build_team(user: user)
+
+    build_notification(team: team, notifiable: tour)
+  end
+
+  def stub_dependencies(tour)
+    allow(Rails.application.routes.url_helpers).to receive(:tour_url).with(tour).and_return('http://test/tours/1')
+
+    allow(I18n).to receive(:t).and_return('message')
+    allow(TelegramBot::Sender).to receive(:call).and_return(true)
   end
 
   describe '#call' do
@@ -49,19 +65,9 @@ RSpec.describe TelegramBot::Tour::OpenedNotifier do
     end
 
     it 'calls Sender service and returns true' do
-      league = build_league
-      tour = build_notifiable(league: league)
-      user = build_user
-      team = build_team(user: user)
-      notification = build_notification(team: team, notifiable: tour)
-
-      allow(Rails.application.routes.url_helpers).to receive(:tour_url).with(tour).and_return('http://test/tours/1')
-      allow(I18n).to receive(:t).and_return('message')
-      allow(TelegramBot::Sender).to receive(:call).and_return(true)
-
-      result = described_class.call(notification)
-
-      expect(result).to be(true)
+      notification = build_valid_notification
+      stub_dependencies(notification.notifiable)
+      expect(described_class.call(notification)).to be(true)
     end
   end
 end
