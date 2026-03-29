1e94fea69ac7d4cb9178e47f0b409fe1733dbc71
Bohdan Chaban <b.chaban.91@gmail.com>
2026-03-29 15:10:20 +0300

    Add mailgun for staging

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -67,21 +67,20 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
-  config.action_mailer.delivery_method = :test
+  config.action_mailer.delivery_method = :smtp
 
   config.action_mailer.show_previews = true
 
   config.action_mailer.default_url_options = { host: 'staging.mantrafootball.org', protocol: 'https' }
 
-  # SMTP settings for gmail
-  # config.action_mailer.smtp_settings = {
-  #   :address              => 'smtp.gmail.com',
-  #   :port                 => 587,
-  #   :user_name            => ENV['GMAIL_USERNAME'],
-  #   :password             => ENV['GMAIL_PASSWORD'],
-  #   :authentication       => 'plain',
-  #   :enable_starttls_auto => true
-  # }
+  config.action_mailer.smtp_settings = {
+    :address              => 'smtp.mailgun.org',
+    :port                 => 587,
+    :user_name            => ENV['MAILGUN_SMTP_LOGIN'],
+    :password             => ENV['MAILGUN_SMTP_PASSWORD'],
+    :authentication       => 'plain',
+    :enable_starttls_auto => true
+  }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
