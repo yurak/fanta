8aa0b7e1c99cfcf69e4ce0b3b9072848b00a378e
Bohdan Chaban <b.chaban.91@gmail.com>
2026-04-03 18:44:39 +0300

    Fix user confirmation

--- app/controllers/users/confirmations_controller.rb
+++ app/controllers/users/confirmations_controller.rb
@@ -4,6 +4,21 @@ module Users
       @email = User.find(params[:user])&.email
     end
 
+    def show
+      self.resource = resource_class.confirm_by_token(params[:confirmation_token])
+
+      if resource.errors[:email].include?(I18n.t('errors.messages.already_confirmed'))
+        if resource.encrypted_password.blank?
+          redirect_to edit_password_path(resource, reset_password_token: resource.send(:set_reset_password_token))
+        else
+          redirect_to new_user_session_path, alert: t('devise.confirmations.already_confirmed')
+        end
+        return
+      end
+
+      super
+    end
+
     protected
 
     def after_confirmation_path_for(_resource_name, resource)
