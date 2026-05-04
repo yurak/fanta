3472fec351859d98e7077c56e1e9ab387ee0e617
Bohdan Chaban <b.chaban.91@gmail.com>
2026-05-04 11:24:38 +0300

    Fix migrations for pg

--- db/migrate/20200712181215_create_tournament_matches.rb
+++ db/migrate/20200712181215_create_tournament_matches.rb
@@ -2,8 +2,8 @@ class CreateTournamentMatches < ActiveRecord::Migration[5.2]
   def change
     create_table :tournament_matches do |t|
       t.references :tournament_round, foreign_key: true
-      t.references :host_club, foreign_key: true
-      t.references :guest_club, foreign_key: true
+      t.references :host_club, foreign_key: { to_table: :clubs }
+      t.references :guest_club, foreign_key: { to_table: :clubs }
       t.integer :host_score
       t.integer :guest_score
 
