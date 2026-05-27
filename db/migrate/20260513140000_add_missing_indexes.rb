87f130ad77173158643c793056698691ef5d7b17
Bohdan Chaban <b.chaban.91@gmail.com>
2026-05-27 21:56:33 +0300

    Fix migration

--- db/migrate/20260513140000_add_missing_indexes.rb
+++ db/migrate/20260513140000_add_missing_indexes.rb
@@ -1,28 +1,28 @@
 class AddMissingIndexes < ActiveRecord::Migration[6.1]
   def change
-    add_index :match_players, :lineup_id
+    add_index :match_players, :lineup_id, if_not_exists: true
 
-    add_index :lineups, :tour_id
-    add_index :lineups, :team_id
+    add_index :lineups, :tour_id, if_not_exists: true
+    add_index :lineups, :team_id, if_not_exists: true
 
-    add_index :substitutes, :main_mp_id
-    add_index :substitutes, :reserve_mp_id
-    add_index :substitutes, :in_rp_id
-    add_index :substitutes, :out_rp_id
+    add_index :substitutes, :main_mp_id, if_not_exists: true
+    add_index :substitutes, :reserve_mp_id, if_not_exists: true
+    add_index :substitutes, :in_rp_id, if_not_exists: true
+    add_index :substitutes, :out_rp_id, if_not_exists: true
 
-    add_index :round_players, :club_id
+    add_index :round_players, :club_id, if_not_exists: true
 
-    add_index :players, :club_id
+    add_index :players, :club_id, if_not_exists: true
 
-    add_index :matches, :tour_id
-    add_index :matches, :host_id
-    add_index :matches, :guest_id
+    add_index :matches, :tour_id, if_not_exists: true
+    add_index :matches, :host_id, if_not_exists: true
+    add_index :matches, :guest_id, if_not_exists: true
 
-    add_index :slots, :team_module_id
+    add_index :slots, :team_module_id, if_not_exists: true
 
-    add_index :tournament_matches, :host_club_id
-    add_index :tournament_matches, :guest_club_id
+    add_index :tournament_matches, :host_club_id, if_not_exists: true
+    add_index :tournament_matches, :guest_club_id, if_not_exists: true
 
-    add_index :user_profiles, :user_id
+    add_index :user_profiles, :user_id, if_not_exists: true
   end
 end
