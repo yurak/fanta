261c3c8d0f0aea326643add7ba058a19a48e7f25
Bohdan Chaban <b.chaban.91@gmail.com>
2026-06-02 21:02:45 +0300

    Fix matches local time

--- app/models/tournament_match.rb
+++ app/models/tournament_match.rb
@@ -7,4 +7,12 @@ class TournamentMatch < ApplicationRecord
 
   scope :by_club, ->(club_id) { where('host_club_id = ? OR guest_club_id = ?', club_id, club_id) }
   scope :by_club_and_t_round, ->(club_id, t_round_id) { by_club(club_id).where(tournament_round_id: t_round_id) }
+
+  def utc_datetime
+    return nil if date.blank? || time.blank?
+
+    DateTime.parse("#{date} #{time} UTC")
+  rescue ArgumentError
+    nil
+  end
 end
--- spec/models/tournament_match_spec.rb
+++ spec/models/tournament_match_spec.rb
@@ -35,6 +35,32 @@ RSpec.describe TournamentMatch do
     end
   end
 
+  describe '#utc_datetime' do
+    context 'when date and time are present' do
+      subject(:match) { create(:tournament_match, date: 'JUN 15, 2026', time: '19:00') }
+
+      it 'returns a DateTime in UTC' do
+        expect(match.utc_datetime).to eq(DateTime.parse('2026-06-15 19:00:00 UTC'))
+      end
+
+      it 'returns UTC offset' do
+        expect(match.utc_datetime.utc_offset).to eq(0)
+      end
+    end
+
+    context 'when date is blank' do
+      subject(:match) { create(:tournament_match, date: '', time: '19:00') }
+
+      it { expect(match.utc_datetime).to be_nil }
+    end
+
+    context 'when time is blank' do
+      subject(:match) { create(:tournament_match, date: 'JUN 15, 2026', time: '') }
+
+      it { expect(match.utc_datetime).to be_nil }
+    end
+  end
+
   describe '#missed_players_data' do
     it 'serializes JSON data' do
       tournament_match.update!(missed_players_data: { 'players' => [1, 2] })
